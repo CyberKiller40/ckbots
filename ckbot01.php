2891c7ce65ccd8f121cbce02f80311e704881299
Łukasz "Cyber Killer" Korpalski <cyber_killer@jabster.pl>
2011-04-26 18:51:38 +0200

    próby ustawienia imienia
    --HG--
    extra : convert_revision : cyber_killer%40jabster.pl-20110426165138-z10lzu0miraebemu

--- ckbot01.php
+++ ckbot01.php
@@ -6,25 +6,31 @@ fwrite(STDOUT, "RobotOption USE_NON_BLOCKING 0\n");
 
 $line=trim(fgets(STDIN));
 
-while($line!="GameFinishes")
+if($line=="Initialize 1")
 {
-	$line=trim(fgets(STDIN));
-	
-	if($line=="Initialize 1")
+	print("note: first init, setting name & colour");
+	fwrite(STDOUT, "Name CKbot01\n");
+	fwrite(STDOUT, "Colour 0000FF 0000FE\n");
+
+	while(($line!="GameFinishes")||($line=="Dead"))
 	{
-		fwrite(STDOUT, "Name CKbot01\n");
-		fwrite(STDOUT, "Colour 0000FF 0000FE\n");
+
+		fwrite(STDOUT, "Accelerate 10\n");
+		fwrite(STDOUT, "Shoot 10\n");
+
+		$line=trim(fgets(STDIN));
 	}
-	else
+}
+else
+{
+	while(($line!="GameFinishes")||($line=="Dead"))
 	{
-		//dunno ;-)
+
+		fwrite(STDOUT, "Accelerate 10\n");
+		fwrite(STDOUT, "Shoot 10\n");
+
+		$line=trim(fgets(STDIN));
 	}
-
-fwrite(STDOUT, "Accelerate 10\n");
-fwrite(STDOUT, "Shoot 10\n");
-
-$line=trim(fgets(STDIN));
-
 }
 
 
