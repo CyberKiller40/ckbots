0b7c97374231ece7dd7b651d3a4835c561b875a0
Łukasz "Cyber Killer" Korpalski <cyber_killer@jabster.pl>
2011-04-26 19:03:04 +0200

    próby obrotu
    --HG--
    extra : convert_revision : cyber_killer%40jabster.pl-20110426170304-8w06l0e2ghd1lu2z

--- ckbot01.php
+++ ckbot01.php
@@ -6,9 +6,7 @@ fwrite(STDOUT, "RobotOption USE_NON_BLOCKING 0\n");
 
 $line=trim(fgets(STDIN));
 
-//if($line=="Initialize 1")
-//{
-	print("note: first init, setting name & colour");
+	fwrite(STDOUT, "Print This should set the name CKbot01\n");
 	fwrite(STDOUT, "Name CKbot01\n");
 	fwrite(STDOUT, "Colour 0000FF 0000FE\n");
 
@@ -16,23 +14,11 @@ $line=trim(fgets(STDIN));
 	{
 
 		fwrite(STDOUT, "Accelerate 10\n");
+		sleep(2);
 		fwrite(STDOUT, "Shoot 10\n");
+		fwrite(STDOUT, "RotateAmount 7 15 1\n");
 
 		$line=trim(fgets(STDIN));
 	}
-//}
-/*
-else
-{
-	while(($line!="GameFinishes")||($line=="Dead"))
-	{
-
-		fwrite(STDOUT, "Accelerate 10\n");
-		fwrite(STDOUT, "Shoot 10\n");
-
-		$line=trim(fgets(STDIN));
-	}
-}
-*/
 
 ?>
