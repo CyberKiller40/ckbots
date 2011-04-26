bd7bd0b10e71a80f5b55212194815acf3a9b5889
Łukasz "Cyber Killer" Korpalski <cyber_killer@jabster.pl>
2011-04-26 19:12:43 +0200

    kolejna próba
    --HG--
    extra : convert_revision : cyber_killer%40jabster.pl-20110426171243-8xmnbybx83sebk3a

--- ckbot01.php
+++ ckbot01.php
@@ -4,12 +4,11 @@
 
 fwrite(STDOUT, "RobotOption USE_NON_BLOCKING 0\n");
 
-$line="";
-
-while($line!="Initialize 1")
+do
 {
 	$line=trim(fgets(STDIN));
 }
+while($line!="Initialize 1");
 
 	fwrite(STDOUT, "Name CKbot01\n");
 	fwrite(STDOUT, "Colour 0000FF 0000FE\n");
